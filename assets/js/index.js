e3d044a606f64b20221e56baf1b36f982000ea4e
Dev avology <devavology12@gmail.com>
2024-08-02 19:24:17 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,37 +10,61 @@ $(document).ready(function () {
   // Menu Toggle JS End
 
 
+// //Cookies JS Start
 
-
-
-// Dropdown JS Start
-jQuery(document).ready(function (e) {
-  function t(t) {
-      e(t).bind("click", function (t) {
-          t.preventDefault();
-          e(this).parent().fadeOut()
-      })
+function SetCookie(cookieName, cookieValue, nDays) {
+  "use strict";
+  var today = new Date();
+  var expire = new Date();
+  if (nDays == null || nDays == 0) nDays = 1;
+  expire.setTime(today.getTime() + 3600000 * 24 * nDays);
+  document.cookie =
+    cookieName +
+    "=" +
+    escape(cookieValue) +
+    ";expires=" +
+    expire.toGMTString() +
+    "; path=/";
+}
+function ReadCookie(cookieName) {
+  "use strict";
+  var theCookie = " " + document.cookie;
+  var ind = theCookie.indexOf(" " + cookieName + "=");
+  if (ind == -1) ind = theCookie.indexOf(";" + cookieName + "=");
+  if (ind == -1 || cookieName == "") return "";
+  var ind1 = theCookie.indexOf(";", ind + 1);
+  if (ind1 == -1) ind1 = theCookie.length;
+  return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
+}
+function DeleteCookie(cookieName) {
+  "use strict";
+  var today = new Date();
+  var expire = new Date() - 30;
+  expire.setTime(today.getTime() - 3600000 * 24 * 90);
+  document.cookie =
+    cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
+}
+function AcceptCookies() {
+  SetCookie("sgCookies", true, 30);
+  jQuery("#cookie-bar").hide();
+}
+$(document).ready(function (e) {
+  var toTop = $(
+    "<div id='cookie-bar'><div id='contentcookie'><p>    BetYouRoom is your premier hotel comparison site for the Czech Republic. To enhance your browsing experience and provide you with tailored services, we use cookies on our website. If you would like to know more information about Cookie Policy, please review our Privacy Policy there you will find a better explanation regarding our cookies.</p><button id='Cookie' tabindex=1 onclick='AcceptCookies();'>Accept </button><div class='clear'></div></div></div>"
+  ).appendTo("body");
+  if (!ReadCookie("sgCookies")) {
+    //If the cookie has not been set
+    jQuery("#cookie-bar").show();
+  } else {
+    jQuery("#cookie-bar").hide();
   }
-  e(".dropdown-toggle").click(function () {
-      var t = e(this).parents(".button-dropdown").children(".dropdown").is(":hidden");
-      e(".button-dropdown .dropdown").hide();
-      e(".button-dropdown .dropdown-toggle").removeClass("dropping");
-      if (t) {
-          e(this).parents(".button-dropdown").children(".dropdown").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("dropping")
-      }
-  });
-  e(document).bind("click", function (t) {
-      var n = e(t.target);
-      if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown").hide();
-  });
-  e(document).bind("click", function (t) {
-      var n = e(t.target);
-      if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("dropping");
-  })
 });
-// Dropdown JS End
+
+// //Cookies JS Start
 
 
+
+// Header scroll js Start
 $(document).ready(function(){
   $(window).scroll(function(){
     var header = $("header");
@@ -48,5 +72,15 @@ $(document).ready(function(){
   });
 });
 
+// Header scroll js End
 
 AOS.init();
+
+
+// Dropdown JS Start
+$(document).ready(function(){
+  $(".button-dropdown").click(function(){
+    $(this).find(".dropdown").slideToggle();
+  });
+});
+// Dropdown JS End
